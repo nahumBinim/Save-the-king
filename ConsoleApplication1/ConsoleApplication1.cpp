e7b21987abe34be0c808abbc4992c1f601860e91
bg <bg@DESKTOP-UBK1JCG>
2016-11-10 22:39:20 +0200

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int i = 1;
+	std::cout << "hello world"<< endl;
+    return 0;
+}
+
